76ce218d88a36215c776b9ac89a29994dc5fdbf4
mohammadtigons <122691572+mohammadtigons@users.noreply.github.com>
2023-05-29 16:21:57 +0330

    Update settings.py

--- bookweb/settings.py
+++ bookweb/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -25,7 +24,7 @@ SECRET_KEY = 'django-insecure-5vzuy(ch!zeyx04aajv!w8ofr5d7s@#35h!7n-xu3_@-ty&wow
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["http://blackhill360.runflare.run/"]
 
 
 # Application definition
